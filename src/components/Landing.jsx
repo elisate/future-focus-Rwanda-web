e47b6168f647ae4644880195f5cc482b19986776
elisate <dushimiyimanaelisa@gmail.com>
2024-07-27 11:21:28 +0200

    fix some deployment

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -7,22 +7,32 @@ function Landing() {
     {
       courseImage: "/C1.jpeg",
       buttonTitle: "Coding",
+      programContent:
+        "Robotics is a fascinating educational tool that brings to life abstract concepts in Science, Technology, Engineering, & Mathematics. Students learn to build intricate robots to solve complex challenges, acquire technical, scientific, & other skills that are critical to be successful in the 21st century such as deep-thinking, problem-solving, creativity, teamwork, communication & collaboration.",
     },
     {
       courseImage: "/ro1.jpg",
       buttonTitle: "Robotics",
+      programContent:
+        "Robotics is a fascinating educational tool that brings to life abstract concepts in Science, Technology, Engineering, & Mathematics. Students learn to build intricate robots to solve complex challenges, acquire technical, scientific, & other skills that are critical to be successful in the 21st century such as deep-thinking, problem-solving, creativity, teamwork, communication & collaboration.",
     },
     {
       courseImage: "/pr1.jpg",
       buttonTitle: "3D Designing & 3D Printing",
+      programContent:
+        "Robotics is a fascinating educational tool that brings to life abstract concepts in Science, Technology, Engineering, & Mathematics. Students learn to build intricate robots to solve complex challenges, acquire technical, scientific, & other skills that are critical to be successful in the 21st century such as deep-thinking, problem-solving, creativity, teamwork, communication & collaboration.",
     },
     {
       courseImage: "/P1.jpg",
       buttonTitle: "Electronics & Embedded Systems",
+      programContent:
+        "Robotics is a fascinating educational tool that brings to life abstract concepts in Science, Technology, Engineering, & Mathematics. Students learn to build intricate robots to solve complex challenges, acquire technical, scientific, & other skills that are critical to be successful in the 21st century such as deep-thinking, problem-solving, creativity, teamwork, communication & collaboration.",
     },
     {
       courseImage: "/R1.jpeg",
       buttonTitle: "Mathematics of Problems Solving",
+      programContent:
+        "Robotics is a fascinating educational tool that brings to life abstract concepts in Science, Technology, Engineering, & Mathematics. Students learn to build intricate robots to solve complex challenges, acquire technical, scientific, & other skills that are critical to be successful in the 21st century such as deep-thinking, problem-solving, creativity, teamwork, communication & collaboration.",
     },
   ];
 
@@ -57,6 +67,45 @@ function Landing() {
     setCurrentCardIndex(pageIndex * cardsPerPage);
   };
 
+  const cardServices = [
+    {
+      cardImage: "/C1.jpeg",
+      cardTitle: "In-School Program",
+      cardDescription:
+        "The STEM activities 100% match REB's Curriculum, we are building partnerships with different schools in region so that STEM programs gets into schools basing on Singapore's Teaching Methodology.",
+    },
+    {
+      cardImage: "/D1.jpeg",
+      cardTitle: "After School Program",
+      cardDescription:
+        "The STEM activities 100% match REB's Curriculum, we are building partnerships with different schools in region so that STEM programs gets into schools basing on Singapore's Teaching Methodology.",
+    },
+    {
+      cardImage: "/P1.jpg",
+      cardTitle: "Online Learning",
+      cardDescription:
+        "The STEM activities 100% match REB's Curriculum, we are building partnerships with different schools in region so that STEM programs gets into schools basing on Singapore's Teaching Methodology.",
+    },
+    {
+      cardImage: "/pr1.jpg",
+      cardTitle: "Centered Learning",
+      cardDescription:
+        "The STEM activities 100% match REB's Curriculum, we are building partnerships with different schools in region so that STEM programs gets into schools basing on Singapore's Teaching Methodology.",
+    },
+    {
+      cardImage: "/P1.jpg",
+      cardTitle: "Online Learning",
+      cardDescription:
+        "The STEM activities 100% match REB's Curriculum, we are building partnerships with different schools in region so that STEM programs gets into schools basing on Singapore's Teaching Methodology.",
+    },
+    {
+      cardImage: "/ro1.jpg",
+      cardTitle: "Home Coaching",
+      cardDescription:
+        "The STEM activities 100% match REB's Curriculum, we are building partnerships with different schools in region so that STEM programs gets into schools basing on Singapore's Teaching Methodology.",
+    },
+  ];
+
   return (
     <div className="landingContainer">
       <div className="landSectiona">
@@ -84,6 +133,7 @@ function Landing() {
                   <img src={item.courseImage} className="cardCourseImagea" />
                 </div>
                 <div className="textcarda">{item.buttonTitle}</div>
+                <div className="prContent">{item.programContent}</div>
               </div>
             ))}
           </div>
@@ -100,6 +150,22 @@ function Landing() {
             ></span>
           ))}
         </div>
+      </div>
+      <div className="ctitle">Join Us For</div>
+      <div className="landingc">
+        {cardServices.map((item, index) => (
+          <div  className="landccards" key={index}>
+            <div >
+              <img
+                src={item.cardImage}
+                alt="card images"
+                className="serviceCardimage"
+              />
+            </div>
+            <div className="cardTitle">{item.cardTitle}</div>
+            <div className="cardDesc">{item.cardDescription}</div>
+          </div>
+        ))}
       </div>
     </div>
   );
