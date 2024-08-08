b48ea59350c3202b3efef1e6995dda436ef27d83
elisate <dushimiyimanaelisa@gmail.com>
2024-08-09 00:27:37 +0200

    fix axios errors

--- src/fetch/ContextProvider.jsx
+++ src/fetch/ContextProvider.jsx
@@ -1,6 +1,6 @@
 import { useContext, useEffect, useState, createContext } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+
 
 // Create context
 const statement = createContext();
